5724ef82d14b298616f105a2d782335e814a1977
chih2020hao <azcwcgwb0108@gmail.com>
2020-11-16 16:03:14 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCheck, faPen, faTimes, faUndo, faPlay, faPause, faStepForward } from '@fortawesome/free-solid-svg-icons'
 
 Vue.config.productionTip = false
+
+library.add(faCheck, faPen, faTimes, faUndo, faPlay, faPause, faStepForward)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
---
+++ src/views/Settings.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+  #settings
+    b-container
+      b-row
+        b-col(cols="12")
+          b-table(:items="items" :fields="fields" @row-clicked="selectAlarm")
+            template(#cell(file)="data")
+              audio(controls :src="'./sounds/'+data.item.file")
+            template(#cell(select)="data")
+              font-awesome-icon(v-if="data.item.file === alarm" :icon="['fas', 'check']")
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: '土撥鼠',
+          file: 'ahhhhhhhh.mp3'
+        },
+        {
+          name: '呷土豆',
+          file: 'jatoudoau.mp3'
+        },
+        {
+          name: '江西南',
+          file: 'johncena.mp3'
+        },
+        {
+          name: 'dejavu',
+          file: 'dejavu2018.mp3'
+        },
+        {
+          name: '好~~~',
+          file: 'hao.mp3'
+        },
+        {
+          name: '歐郎',
+          file: 'niggaeatfruit.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'file',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  },
+  computed: {
+    alarm () {
+      return this.$store.state.alarm
+    }
+  },
+  methods: {
+    selectAlarm (item) {
+      this.$store.commit('selectAlarm', item.file)
+    }
+  }
+}
+</script>
