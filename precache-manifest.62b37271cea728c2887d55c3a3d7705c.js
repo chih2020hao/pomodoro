1f2fbb05ad12262c0ec33e899b7250bba842b0a1
chih2020hao <70134576+chih2020hao@users.noreply.github.com>
2020-11-16 08:05:36 +0000

    Deploying to gh-pages from master 5724ef82d14b298616f105a2d782335e814a1977

---
+++ precache-manifest.62b37271cea728c2887d55c3a3d7705c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c129749775dc7841d4e",
+    "url": "css/chunk-vendors.713ba136.css"
+  },
+  {
+    "revision": "0cb90b56f9efee6fc245e850c8c743cd",
+    "url": "index.html"
+  },
+  {
+    "revision": "52c5460df6d28948b212",
+    "url": "js/app.0a9d91db.js"
+  },
+  {
+    "revision": "9c129749775dc7841d4e",
+    "url": "js/chunk-vendors.f900ca0b.js"
+  },
+  {
+    "revision": "09c4e2ba3b7137c07995",
+    "url": "js/list.92a3c14f.js"
+  },
+  {
+    "revision": "92291067737054a10276",
+    "url": "js/settings.84757fae.js"
+  },
+  {
+    "revision": "982672f70a8ae0b8927c7e9ed7d470cb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "75893f09b57db208f4a1c1d6c2596a79",
+    "url": "sounds/ahhhhhhhh.mp3"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "sounds/alarm1.mp3"
+  },
+  {
+    "revision": "4221c71a5d29252f2134f435e85f6152",
+    "url": "sounds/dejavu2018.mp3"
+  },
+  {
+    "revision": "237b7c1a1cbf7783903ad13c88b9a15a",
+    "url": "sounds/hao.mp3"
+  },
+  {
+    "revision": "6c8624d8411f3990c55eb814b2e20436",
+    "url": "sounds/jatoudoau.mp3"
+  },
+  {
+    "revision": "303147c636ca32d0eb711101a925a411",
+    "url": "sounds/johncena.mp3"
+  },
+  {
+    "revision": "ce81f7a779b98957caeac29a907318e7",
+    "url": "sounds/niggaeatfruit.mp3"
+  }
+]);
